9183dcf1c205efa9d8ea81aa067493a610d5472d
SanBingYouYong <rayzhang707@gmail.com>
2023-09-22 14:45:57 +0100

    formatted tests; added ground truth counts for files

--- 2023/scripts/tests.py
+++ 2023/scripts/tests.py
@@ -8,41 +8,60 @@ if PREFIX_2023[-4:] == "data":
     PREFIX_2023 = os.path.join(
         PREFIX_2023, "2023"
     )
+GT_COUNTS = {
+    "total": 48 + 24 + 40,
+    "respecks": 24 + 24 + 40,
+    "thingys": 24
+}
 
 
 # FUNCTIONS
-def integrity_check(uun: str) -> bool:
-    '''
-    Checks if all files under this dir belongs to the right student.
-    '''
-    full_path = os.path.join(PREFIX_2023, uun)
-    files = os.listdir(full_path)
-    for file in files:
-        if uun not in file:
-            return False
-    return True
+class DataExaminer():
+    def __init__(self, uun: str, path_prefix: str=PREFIX_2023, 
+                 gt_counts: dict=GT_COUNTS) -> None:
+        self.uun = uun
+        self.full_path = os.path.join(PREFIX_2023, uun)
+        self.files = os.listdir(self.full_path)
+        self.gt_counts = gt_counts
+    
+    def all_checks(self) -> bool:
+        integrity = self.integrity_check()
+        counts = self.count_files()
 
-def count_files(uun: str) -> Dict[str,int]:
-    results = {}
-    full_path = os.path.join(PREFIX_2023, uun)
-    files = os.listdir(full_path)
-    results["total"] = len(files)
-    respecks = 0
-    thingys = 0
-    for file in files:
-        if "Respeck" in file:
-            respecks += 1
-        elif "Thingy" in file:
-            thingys += 1
-        else:
-            raise AssertionError(f"{file} is neither respeck nor thingy recording.")
-    results["respeck"] = respecks
-    results["thingy"] = thingys
-    return results
+        checks = [integrity, counts == self.gt_counts]
+        if all(checks):
+            return True
+        return False
+
+    def integrity_check(self) -> bool:
+        '''
+        Checks if all files under this dir belongs to the right student.
+        '''
+        for file in self.files:
+            if self.uun not in file:
+                return False
+        return True
+
+    def count_files(self) -> Dict[str,int]:
+        results = {}
+        results["total"] = len(self.files)
+        respecks = 0
+        thingys = 0
+        for file in self.files:
+            if "Respeck" in file:
+                respecks += 1
+            elif "Thingy" in file:
+                thingys += 1
+            else:
+                raise AssertionError(f"{file} is neither respeck nor thingy recording.")
+        results["respeck"] = respecks
+        results["thingy"] = thingys
+        return results
 
 
 if __name__ == "__main__":
-    student = "s0247783"
-    print(integrity_check(student))
-    res = count_files(student)
-    print(res)
+    student = "s2047783"
+    examiner = DataExaminer(student)
+    print(examiner.all_checks())
+    # integrity = examiner.integrity_check()
+    # counts = examiner.count_files()
