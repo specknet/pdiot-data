a96211a03069826d6974a278c6a24eb107f38807
SanBingYouYong-S.Zhang <s2047783@ed.ac.uk>
2023-09-27 13:03:22 +0100

    plot update working

--- 2023/scripts/trim_interface.py
+++ 2023/scripts/trim_interface.py
@@ -9,10 +9,13 @@ import matplotlib.ticker as ticker
 
 from viewer import DataViewer
 import viewer_utils
+import os
 
 class MplCanvas(FigureCanvasQTAgg):
 
     def __init__(self, dataframe, plot_title):
+        self.dataframe = dataframe
+        self.plot_title = plot_title
         # taken from and modified based plot_data()
         # Calculate the number of data points in your dataset
         num_data_points = len(dataframe)
@@ -83,6 +86,60 @@ class MplCanvas(FigureCanvasQTAgg):
         # Add vertical grid lines (gridlines along the x-axis)
         self.ax1.grid(axis="x", linestyle="--", linewidth=line_width)
         self.ax2.grid(axis="x", linestyle="--", linewidth=line_width)
+    
+    def update_plot(self, new_data):
+        self.dataframe = new_data
+        # Clear existing plots
+        self.ax1.clear()
+        self.ax2.clear()
+
+        # Update the plots with new data
+        line_width = 2
+        self.ax1.plot(new_data["accel_x"], label="accel_x", linewidth=line_width)
+        self.ax1.plot(new_data["accel_y"], label="accel_y", linewidth=line_width)
+        self.ax1.plot(new_data["accel_z"], label="accel_z", linewidth=line_width)
+        self.ax1.legend()
+
+        self.ax1.set_title(
+            f"{new_data['sensor_type'].values[0]} - {new_data['activity_type'].values[0]} \n Accelerometer data"
+        )
+
+        self.ax2.plot(new_data["gyro_x"], label="gyro_x", linewidth=line_width)
+        self.ax2.plot(new_data["gyro_y"], label="gyro_y", linewidth=line_width)
+        self.ax2.plot(new_data["gyro_z"], label="gyro_z", linewidth=line_width)
+        self.ax2.legend()
+
+        num_xticks = len(new_data) // 10
+        self.ax1.xaxis.set_major_locator(ticker.MaxNLocator(num_xticks))
+        self.ax2.xaxis.set_major_locator(ticker.MaxNLocator(num_xticks))
+
+        fnt_size = 9
+        fnt_size2 = 6
+
+        self.ax2.set_xlabel(
+            "Data point no", fontsize=fnt_size
+        )  # Adjust fontsize for the x-axis label
+        self.ax1.set_ylabel(
+            "Acceleration", fontsize=fnt_size
+        )  # Adjust fontsize for the y-axis label
+        self.ax2.set_ylabel("Gyroscope", fontsize=fnt_size)
+
+        # Adjust fontsize of individual ticks on the x-axis and y-axis for both subplots
+        self.ax1.tick_params(axis="both", labelsize=fnt_size2)
+        self.ax2.tick_params(axis="both", labelsize=fnt_size2)
+
+        # Rotate x-axis tick labels by 45 degrees for both subplots
+        self.ax1.tick_params(axis="x", labelrotation=45)
+        self.ax2.tick_params(axis="x", labelrotation=45)
+
+        self.ax1.set_title(self.plot_title, size=fnt_size)
+
+        # Add vertical grid lines (gridlines along the x-axis)
+        self.ax1.grid(axis="x", linestyle="--", linewidth=line_width)
+        self.ax2.grid(axis="x", linestyle="--", linewidth=line_width)
+
+        # Redraw the canvas
+        self.draw()
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -90,16 +147,16 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        student = "s2047783"
-        datafile = "Respeck_s2047783_Ascending stairs_Normal_21-09-2023_12-25-57.csv"
-        viewer = DataViewer(student)
-        data = viewer.load_data(datafile)
+        self.student = "s2047783"
+        self.datafile = "Respeck_s2047783_Ascending stairs_Normal_21-09-2023_12-25-57.csv"
+        self.viewer = DataViewer(self.student)
+        self.data = self.viewer.load_data(self.datafile)
 
         # Create the matplotlib FigureCanvas object,
         # which defines a single set of axes as self.axes.
         # self.sc = MplCanvas(self, width=5, height=4, dpi=100)
         # self.sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-        self.sc = MplCanvas(data, datafile)
+        self.sc = MplCanvas(self.data, self.datafile)
 
         # Create labels and input boxes for integers
         self.label1 = QtWidgets.QLabel("Trim Start:")
@@ -157,6 +214,18 @@ class MainWindow(QtWidgets.QMainWindow):
         # This method can be populated to carry out some action with the recorded values
         print("Processing Recorded Values")
         print(self.recorded_values)
+
+        # df_respeck['ind'] = df_respeck.index
+        self.data['ind'] = self.data.index
+        to_trim = len(self.recorded_values)
+        for i in range(int(to_trim)):
+            range_trim_start = int(self.recorded_values[i][0])
+            range_trim_end = int(self.recorded_values[i][1])
+            # df_respeck = df_respeck[~((df_respeck['ind'] >= range_trim_start) & (df_respeck['ind'] <= range_trim_end))]
+            self.data = self.data[~((self.data['ind'] >= range_trim_start) & (self.data['ind'] <= range_trim_end))]
+        # Update the plot in self.sc with the modified data
+        self.sc.update_plot(self.data)
+
 
     def update_recorded_values_text(self):
         # Update the QTextEdit widget to display recorded values
