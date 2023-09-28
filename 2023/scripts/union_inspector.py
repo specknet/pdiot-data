6c5a384078f6d4b591c95cc9e7671283b6c89efa
SanBingYouYong-S.Zhang <s2047783@ed.ac.uk>
2023-09-28 23:18:55 +0100

    area division working

--- 2023/scripts/union_inspector.py
+++ 2023/scripts/union_inspector.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -9,28 +9,32 @@ class MyWindow(QMainWindow):
 
     def initUI(self):
         # 设置主窗口标题
-        self.setWindowTitle('PyQt5 Template')
+        self.setWindowTitle('Three Vertical Areas')
 
         # 设置窗口尺寸
-        self.setGeometry(0, 0, 500, 500)
+        self.setGeometry(100, 100, 400, 300)
 
-        # 创建一个标签
-        self.label = QLabel('Hello, PyQt5!', self)
-        self.label.setGeometry(150, 150, 200, 50)
+        # 创建一个主窗口中的中心部件
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
-        # 创建一个按钮
-        self.button = QPushButton('Click Me', self)
-        self.button.setGeometry(150, 200, 100, 30)
+        # 创建一个垂直布局
+        layout = QVBoxLayout(central_widget)
 
-        # 按钮点击事件处理
-        self.button.clicked.connect(self.on_button_click)
+        # 创建第一个区域，大小比例为3
+        label1 = QLabel('Area 1 (3x)', self)
+        layout.addWidget(label1, 6)  # 3表示分配更多的空间
 
-    def on_button_click(self):
-        # 处理按钮点击事件
-        self.label.setText('Button Clicked!')
+        # 创建第二个区域，大小比例为1
+        label2 = QLabel('Area 2 (1x)', self)
+        layout.addWidget(label2, 2)
+
+        # 创建第三个区域，大小比例为0.5
+        label3 = QLabel('Area 3 (0.5x)', self)
+        layout.addWidget(label3, 1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyWindow()
-    window.showMaximized()
+    window.show()
     sys.exit(app.exec_())
